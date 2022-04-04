ac6609bb5b1cc0d28c5bd8f9f5358d1996a0a9c0
ToshanChowdhury <102951333+ToshanChowdhury@users.noreply.github.com>
2022-04-04 12:04:48 +0300

    Add files via upload

---
+++ Computers/CloudComputer.py
@@ -0,0 +1,18 @@
+projects = "Shell OS", "Vira"
+links = "https://1drv.ms/u/s!Ag6ZJEd83-sgg-FPODJ43UEx7zc-Gg?e=YTTd2T", "https://1drv.ms/u/s!Ag6ZJEd83-sghJJ0YBdFHqFnJH-7qQ?e=a8xmIm"
+author = "tanmoy_ch@outlook.com", "toshan-ch@outlook.com"
+web = False
+cloud_computer = True
+user = 5612091
+profile = "TOSHAN CHOWDHURY"
+password = "Taken4mu!@"
+domain = ["Python Cloud", "Shell OS", "GMTRepairToolOS", "Python OS"]
+company = ["WinSpire", "WinLow"]
+computer_ID = 7812466
+
+data = ["Banking NO: 1982329",
+"Social Security ID: 9012312",
+"Sever ID: ui5590mu79-975qw13h69-17gh619969-81533u891q",
+"Session ID: "+str(user),
+"Domain Name: "+str(domain),
+"Company Name: "+str(company)]
